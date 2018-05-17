1072caffc7ae8a4ecc904e91174641191ea4111b
Spencer Carli <spencercarli@gmail.com>
2018-05-17 14:08:07 -0500

    Get rid of yellow box warnings

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry, YellowBox } from 'react-native';
 import App from './App/index';
+
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
 
 AppRegistry.registerComponent('CurrencyConverter', () => App);
