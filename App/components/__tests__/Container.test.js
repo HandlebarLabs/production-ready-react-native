91a09b41132cbf380ed6178c726a8f904514dc3d
Spencer Carli <spencercarli@gmail.com>
2018-05-21 16:13:26 -0500

    Snapshot test for container

--- App/components/__tests__/Container.test.js
+++ App/components/__tests__/Container.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { View } from 'react-native';
 
 import { Container, styles } from '../Container';
 
@@ -10,4 +11,18 @@ it('renders successfully without children', () => {
 
 it('exports a styles object', () => {
   expect(typeof styles).toBe('object');
+});
+
+it('renders children props', () => {
+  const rendered = renderer
+    .create(<Container>
+      <View />
+            </Container>)
+    .toJSON();
+  expect(rendered).toMatchSnapshot();
+});
+
+it('uses the specified backgroundColor if provided', () => {
+  const rendered = renderer.create(<Container backgroundColor="red" />).toJSON();
+  expect(rendered).toMatchSnapshot();
 });
