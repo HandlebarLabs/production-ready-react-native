40cc01664c8cc5b03d069fae0f1845ad2d907541
Spencer Carli <spencercarli@gmail.com>
2018-05-21 16:33:45 -0500

    Convert reducer tests to use snapshots

--- App/reducers/__tests__/currencies.test.js
+++ App/reducers/__tests__/currencies.test.js
@@ -1,31 +1,10 @@
 import reducer from '../currencies';
 import { getInitialConversion } from '../../actions/currencies';
 
-const initialState = {
-  baseCurrency: 'USD',
-  quoteCurrency: 'GBP',
-  amount: 100,
-  conversions: {},
-  error: null,
-};
-
 it('sets initial state', () => {
-  const expected = { ...initialState };
-  const actual = reducer(undefined, {});
-  expect(actual).toEqual(expected);
+  expect(reducer(undefined, {})).toMatchSnapshot();
 });
 
 it('sets nested data on initial fetch', () => {
-  const expected = {
-    ...initialState,
-    conversions: {
-      USD: {
-        date: '',
-        isFetching: true,
-        rates: {},
-      },
-    },
-  };
-  const actual = reducer(undefined, getInitialConversion());
-  expect(actual).toEqual(expected);
+  expect(reducer(undefined, getInitialConversion())).toMatchSnapshot();
 });
