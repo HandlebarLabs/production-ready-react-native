fd478a22e73187ed5c3afcb18b3b3bb6d7424160
Spencer Carli <spencercarli@gmail.com>
2018-05-21 16:22:31 -0500

    Rewrite action tests using actions

--- App/actions/__tests__/currencies.test.js
+++ App/actions/__tests__/currencies.test.js
@@ -2,28 +2,16 @@ import { getInitialConversion, changeCurrencyAmount } from '../currencies';
 
 describe('getInitialConversion', () => {
   it('creates a properly formatted action', () => {
-    const expected = { type: 'GET_INITIAL_CONVERSION' };
-    const actual = getInitialConversion();
-    expect(expected).toEqual(actual);
+    expect(getInitialConversion()).toMatchSnapshot();
   });
 });
 
 describe('changeCurrencyAmount', () => {
   it('creates an action with an amount of 100', () => {
-    const expected = {
-      type: 'CHANGE_CURRENCY_AMOUNT',
-      amount: 100,
-    };
-    const actual = changeCurrencyAmount(100);
-    expect(expected).toEqual(actual);
+    expect(changeCurrencyAmount(100)).toMatchSnapshot();
   });
 
   it('converts a string value to a float', () => {
-    const expected = {
-      type: 'CHANGE_CURRENCY_AMOUNT',
-      amount: 100,
-    };
-    const actual = changeCurrencyAmount('100');
-    expect(expected).toEqual(actual);
+    expect(changeCurrencyAmount('100')).toMatchSnapshot();
   });
 });
